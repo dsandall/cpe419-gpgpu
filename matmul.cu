c458bf08cf669959865a7ff6c6a62b6ec7aa5536
d <dsandall@calpoly.edu>
2025-10-03 14:14:31 -0700

    same issue, refactored

--- matmul.cu
+++ matmul.cu
@@ -15,6 +15,7 @@ float *mat_B;//[MAT_N*MAT_M];
 float *mat_C_cpu;//[MAT_M*MAT_M]; // results in MxM matrix
 float *mat_C_gpu;//[MAT_M*MAT_M]; // results in MxM matrix
 
+/*
 void printMatrix(float* matrix, int rows, int cols) {
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
@@ -23,47 +24,31 @@ void printMatrix(float* matrix, int rows, int cols) {
         printf("\n");
     }
 }
+*/
 
-
-__global__ void matMul_GPU(float* mat_A, float* mat_B, float* mat_C_gpu){
-  int global_thread_id = blockIdx.x*blockDim.x+threadIdx.x;
-  //printf("%d\n", global_thread_id);
-
-  //int num_remainder_threads = MAT_M % 1024;
-  //if (blockIdx.x == blockDim.x -1){
-  //  if (threadIdx.x > num_remainder_threads){
-  //    return;
-  //  }
-  //}
-
+__host__ __device__ inline void matMul_base(int i, float* A, float* B, float* C){
   float current;
-  //for (int i = 0; i < MAT_M; i++) {
-    for (int j = 0; j < MAT_M; j++) {
-      current = 0;
-      for (int k = 0; k < MAT_N; k++) {
-        current += mat_A[global_thread_id*MAT_N + k] * mat_B[k*MAT_M + j];
-      }
-      mat_C_gpu[global_thread_id*MAT_M + j] = current;
+  for (int j = 0; j < MAT_M; j++) {
+    current = 0;
+    for (int k = 0; k < MAT_N; k++) {
+      current += A[i*MAT_M + k] * B[k*MAT_N + j];
     }
-  //}
-}
-
-void matMul_CPU(){
-  float current;
-  for (int i = 0; i < MAT_M; i++) {
-    for (int j = 0; j < MAT_M; j++) {
-      current = 0;
-      for (int k = 0; k < MAT_N; k++) {
-        current += mat_A[i*MAT_M + k] * mat_B[k*MAT_N + j];
-      }
-      mat_C_cpu[i*MAT_M + j] = current;
-    }
+    C[i*MAT_M + j] = current;
   }
 }
 
+void matMul_CPU(){
+  for (int i = 0; i < MAT_M; i++) {
+    matMul_base(i, mat_A, mat_B, mat_C_cpu);
+  }
+}
+
+__global__ void matMul_GPU(float* mat_A, float* mat_B, float* mat_C_gpu){
+  int global_thread_id = blockIdx.x*blockDim.x+threadIdx.x;
+  matMul_base(global_thread_id, mat_A, mat_B, mat_C_gpu);
+}
+
 int main(int argc, char *argv[]) {
-
-
   cudaMallocManaged(&mat_A, MAT_M*MAT_N*sizeof(float));
   cudaMallocManaged(&mat_B, MAT_N*MAT_M*sizeof(float));
   cudaMallocManaged(&mat_C_cpu, MAT_M*MAT_M*sizeof(float));
@@ -100,9 +85,9 @@ int main(int argc, char *argv[]) {
   ////
   ////
 
-
   clock_gettime(CLOCK_MONOTONIC, &begin);
   int num_blocks = (MAT_M + 32 - 1)/32;
+  printf("num blocks is: %d\n", num_blocks);
   matMul_GPU<<<num_blocks,32>>>(mat_A, mat_B, mat_C_gpu);
 
   cudaError_t err = cudaSuccess;
