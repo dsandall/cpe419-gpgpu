bb5f2eb84fea17e115a7045d4b960f1e5da63f16
d <dsandall@calpoly.edu>
2025-10-17 20:54:36 -0700

    working, need to migrate im2col to kernel

--- a4_convolution/convolution.cu
+++ a4_convolution/convolution.cu
@@ -1,13 +1,21 @@
 #include "../helpful.cuh"
+#include <ctime>
 #include <cuda.h>
 #include <cuda_runtime.h>
 
-#define KERNEL_N 7
-#define IMAGE_N 17ULL
+#define KERNEL_N 53
+#define IMAGE_N 301ULL
 #define OUTPUT_N (IMAGE_N - KERNEL_N + 1)
+
+// #define COMPARE_SEQ
+#define COMPARE_IM2COL
 
 // tile size 16 makes our threads per block 256
 #define TILE_SZ 16
+
+const int output_elements =
+    IMAGE_N - (KERNEL_N / 2) +
+    1; // this should be the number of vars in the output
 
 __global__ void simpleConv(float *img, float *kernel, float *imgf, int Nx,
                            int Ny, int kernel_size, int center) {
@@ -18,24 +26,25 @@ __global__ void simpleConv(float *img, float *kernel, float *imgf, int Nx,
   int image_i = output_i + center;
   int image_j = output_j + center;
 
-  if (image_i < (Ny - center) && image_j < Nx - center) {
-    printf("(%d,%d) -> (%d,%d)\n", output_i, output_j, image_i, image_j);
+  bool valid = image_i < (Ny - center) && image_j < Nx - center;
+  if (!valid)
+    return; // diverged
+  // printf("(%d,%d) -> (%d,%d)\n", output_i, output_j, image_i, image_j);
 
-    // MAC each surrounding pixel in the (square) kernel zone
-    float sum = 0;
-    for (int ki = 0; ki < kernel_size; ki++) {
-      for (int kj = 0; kj < kernel_size; kj++) {
-        int ii = image_j + kj - center;
-        int jj = image_i + ki - center;
-        sum += img[jj * Nx + ii] * kernel[ki * kernel_size + kj];
-      }
+  // MAC each surrounding pixel in the (square) kernel zone
+
+  float sum = 0;
+  // TODO: shared mem
+  for (int ki = 0; ki < kernel_size; ki++) {
+    for (int kj = 0; kj < kernel_size; kj++) {
+      int ii = image_j + kj - center;
+      int jj = image_i + ki - center;
+      sum += img[jj * Nx + ii] * kernel[ki * kernel_size + kj];
     }
-    // and store the result in device memory
-    imgf[image_i * Nx + image_j] = sum;
-  } else {
-    printf("DIVERGED: (%d,%d) -> (%d,%d)\n", output_i, output_j, image_i,
-           image_j);
   }
+
+  // and store the result in device memory
+  imgf[output_i * OUTPUT_N + output_j] = sum;
 }
 __host__ void doConv(float *img, float *kernel, float *imgf, int Nx, int Ny,
                      int kernel_size, int center) {
@@ -52,9 +61,82 @@ __host__ void doConv(float *img, float *kernel, float *imgf, int Nx, int Ny,
         }
       }
       // and store the result
-      imgf[i * Nx + j] = sum;
+      // imgf[i * Nx + j] = sum;
+      int out_i = i - center;
+      int out_j = j - center;
+      imgf[out_i * OUTPUT_N + out_j] = sum;
     }
   }
+}
+
+__host__ void im2col_alt(float *input, float *kernel, float *out, int H, int W,
+                         int KH, int KW) {
+  int out_h = H - KH + 1;
+  int out_w = W - KW + 1;
+  int patch = 0;
+
+  int col_h = KH * KW;
+  int col_w = out_h * out_w;
+  float *col = (float *)malloc(sizeof(float) * col_h * col_w);
+
+  for (int i = 0; i < out_h; ++i) {
+    for (int j = 0; j < out_w; ++j) {
+
+      // build col
+      int idx = (i * out_w + j) * (KH * KW);
+      for (int ki = 0; ki < KH; ++ki) {
+        for (int kj = 0; kj < KW; ++kj) {
+          col[idx++] = input[(i + ki) * W + (j + kj)];
+        }
+      }
+
+      // do matmul
+      float sum = 0.0f;
+      int p = i * out_w + j;
+      for (int k = 0; k < col_h; ++k) {
+        sum += kernel[k] * col[k + p * col_h];
+      }
+      out[p] = sum;
+    }
+  }
+
+  free(col);
+}
+
+__host__ void im2col_mm_seq(float *input, float *kernel, float *out, int H,
+                            int W, int KH, int KW) {
+  int out_h = H - KH + 1;
+  int out_w = W - KW + 1;
+  int patch = 0;
+
+  int col_h = KH * KW;
+  int col_w = out_h * out_w;
+  float *col = (float *)malloc(sizeof(float) * col_h * col_w);
+
+  for (int i = 0; i < out_h; ++i) {
+    for (int j = 0; j < out_w; ++j) {
+
+      int idx = patch * (KH * KW);
+      for (int ki = 0; ki < KH; ++ki) {
+        for (int kj = 0; kj < KW; ++kj) {
+          col[idx++] = input[(i + ki) * W + (j + kj)];
+        }
+      }
+      patch++;
+    }
+  }
+
+  // matmul: out = Wᵀ * col
+  // (1 x KH*KW) * (KH*KW x out_h*out_w)
+  for (int p = 0; p < col_w; ++p) {
+    float sum = 0.0f;
+    for (int k = 0; k < col_h; ++k) {
+      sum += kernel[k] * col[k + p * col_h];
+    }
+    out[p] = sum;
+  }
+
+  free(col);
 }
 
 int main() {
@@ -66,9 +148,10 @@ int main() {
   float *h_H = (float *)malloc(size_H);
   float *h_G = (float *)malloc(size_G);
   float *h_G_sequential = (float *)malloc(size_G);
+  float *h_G_im2col = (float *)malloc(size_G);
 
   // Initialize image with random integer values [0, 255]
-  srand(243);
+  srand(time(NULL));
   for (int i = 0; i < IMAGE_N * IMAGE_N; i++)
     h_F[i] = (float)(rand() % 256); // convert to float automatically
 
@@ -82,21 +165,13 @@ int main() {
   cudaEventCreate(&startEvent);
   cudaEventCreate(&stopEvent);
 
-  // setup device
+  // setup device memory
   float *d_F, *d_H, *d_G;
   checkCuda(cudaMalloc((void **)&d_F, size_F), "Alloc F");
   checkCuda(cudaMalloc((void **)&d_H, size_H), "Alloc H");
   checkCuda(cudaMalloc((void **)&d_G, size_G), "Alloc G");
 
-  // TODO: make async later
-  checkCuda(cudaMemcpy(d_F, h_F, size_F, cudaMemcpyHostToDevice), "Memcpy F");
-  checkCuda(cudaMemcpy(d_H, h_H, size_H, cudaMemcpyHostToDevice), "Memcpy H");
-  checkCuda(cudaMemcpy(d_G, h_G, size_G, cudaMemcpyHostToDevice), "Memcpy G");
-
-  const int output_elements =
-      IMAGE_N - (KERNEL_N / 2) +
-      1; // this should be the number of vars in the output
-
+  // setup device threads and blocks
   dim3 dimBlock(TILE_SZ, TILE_SZ); // constant tile size
   dim3 dimGrid(ceilDiv(OUTPUT_N, TILE_SZ),
                ceilDiv(OUTPUT_N, TILE_SZ)); // varied based on image size
@@ -104,22 +179,55 @@ int main() {
   printf("dimblock is %d x %d\n", TILE_SZ, TILE_SZ);
   printf("dimgrid  is %d\n", dimGrid.x);
 
-  // do parallel
+  // do parallel kernel
+  cudaEventRecord(startEvent);
+
+  // TODO: make async later
+  checkCuda(cudaMemcpy(d_F, h_F, size_F, cudaMemcpyHostToDevice), "Memcpy F");
+  checkCuda(cudaMemcpy(d_H, h_H, size_H, cudaMemcpyHostToDevice), "Memcpy H");
+  // checkCuda(cudaMemcpy(d_G, h_G, size_G, cudaMemcpyHostToDevice), "Memcpy
+  // G");
   simpleConv<<<dimGrid, dimBlock>>>(d_F, d_H, d_G, IMAGE_N, IMAGE_N, KERNEL_N,
                                     KERNEL_N / 2);
+
   checkCuda(cudaMemcpy(h_G, d_G, size_G, cudaMemcpyDeviceToHost),
             "Memcpy G back to the host");
 
+  cudaDeviceSynchronize();
+
+  cudaEventRecord(stopEvent);
+  cudaEventSynchronize(stopEvent);
+  cudaEventElapsedTime(&ms, startEvent, stopEvent);
+  printf("time elapsed(ms, parallel): %f\n", ms);
+
+#ifdef COMPARE_IM2COL
+  float *h_Fcolumn = (float *)malloc(size_F);
+
+  cudaEventRecord(startEvent);
+  // im2col_mm_seq(h_F, h_H, h_G_im2col, IMAGE_N, IMAGE_N, KERNEL_N, KERNEL_N);
+  im2col_alt(h_F, h_H, h_G_im2col, IMAGE_N, IMAGE_N, KERNEL_N, KERNEL_N);
+  cudaEventRecord(stopEvent);
+  cudaEventSynchronize(stopEvent);
+  cudaEventElapsedTime(&ms, startEvent, stopEvent);
+  printf("time elapsed(ms, im2col sequential): %f\n", ms);
+
+  // verify
+  checkFloats(OUTPUT_N * OUTPUT_N, h_G, h_G_im2col);
+#endif
+
   // do sequential
+#ifdef COMPARE_SEQ
   cudaEventRecord(startEvent);
   doConv(h_F, h_H, h_G_sequential, IMAGE_N, IMAGE_N, KERNEL_N, KERNEL_N / 2);
   cudaEventRecord(stopEvent);
+  cudaEventSynchronize(stopEvent);
   cudaEventElapsedTime(&ms, startEvent, stopEvent);
   printf("time elapsed(ms, sequential): %f\n", ms);
 
   // verify
-  printArray1D(h_G, h_G_sequential, OUTPUT_N * OUTPUT_N);
+  // printArray1D(h_G, h_G_sequential, OUTPUT_N * OUTPUT_N);
   checkFloats(OUTPUT_N * OUTPUT_N, h_G, h_G_sequential);
+#endif
 
   // Cleanup
   free(h_F);
